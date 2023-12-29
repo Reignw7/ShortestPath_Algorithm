5921943a1849643e8551a5a3570b4d9d6ca75acc
Reignw7 <62434249+Reignw7@users.noreply.github.com>
2023-12-29 11:51:44 +0200

    Update network.java

--- network.java
+++ network.java
@@ -1,38 +1,9 @@
-public class network {
-
+public class Network {
     public int cost;
     public String name;
 
-     public network (String name, int cost){
-
-         this.cost = cost;
-         this.name = name;
-     }
-
-     public int compare(network x, network y) {
-         if (x.cost < y.cost) {
-             return -1;
-         }
-
-         if(x.cost> y.cost){
-             return 1;
-         }
-         else return 0;
-     }
-
-     public int compareTo( network x){
-         if(this.cost < x.cost){
-             return -1;
-         }
-
-         if(this.cost > x.cost){
-             return 1;
-         }
-         else return 0;
-     }
-
-     public void setCost(int cost){
-         this.cost = cost;
-     }
+    public Network(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
 }
-
